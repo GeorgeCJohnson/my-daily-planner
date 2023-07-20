0e3505805d480c0b5037a3d63faf741f66b792fd
George J <Gjohnson.pioneer@gmail.com>
2023-07-20 16:33:18 -0400

    Add time blocks and for loop.

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
     const currentDayElement = document.getElementById("currentDay");
     const today = new Date();
     currentDayElement.textContent = today.toDateString();
+
+  // Generate time blocks for standard business hours
+  const container = document.querySelector(".container");
+  const businessHours = {
+    start: 9,
+    end: 17
+  };
+
+  for (let hour = businessHours.start; hour <= businessHours.end; hour++) {
+    const timeBlock = document.createElement("div");
+    timeBlock.classList.add("time-block");
+    timeBlock.textContent = `${hour}:00`;
