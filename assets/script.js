2bfea8df2bb64fd06cfd03132d152e7fac1f166a
George J <Gjohnson.pioneer@gmail.com>
2023-07-21 15:07:52 -0400

    Remove unneeded code from starter files, add div for the javascript content in html, reformat code for readability.

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,54 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Display the current day at the top of the calendar
-    const currentDayElement = document.getElementById("currentDay");
-    const today = new Date();
-    currentDayElement.textContent = today.toDateString();
+  // Display the current day at the top of the calendar
+  const currentDayElement = document.getElementById("currentDay");
+  const today = new Date();
+  currentDayElement.textContent = today.toDateString();
 
-// Generate time blocks for standard business hours
-const container = document.querySelector(".container");
-const businessHours = {
+  // Generate time blocks based on the criteria in the README.
+  const container = document.getElementById("container");
+  const businessHours = {
     start: 9,
     end: 17
-};
+  };
 
-for (let hour = businessHours.start; hour <= businessHours.end; hour++) {
+  for (let hour = businessHours.start; hour <= businessHours.end; hour++) {
     const timeBlock = document.createElement("div");
     timeBlock.classList.add("time-block");
     timeBlock.textContent = `${hour}:00`;
 
-// Color-code the time blocks based on past, present, or future
+    // Color-code the time blocks based on past, present, or future
     const currentTime = new Date().getHours();
     if (hour < currentTime) {
-        timeBlock.classList.add("past");
-        } 
-        
-        else if (hour === currentTime) {
-          timeBlock.classList.add("present");
-        } 
-        
-        else {
-          timeBlock.classList.add("future");
-        }
+      timeBlock.classList.add("past");
+    } else if (hour === currentTime) {
+      timeBlock.classList.add("present");
+    } else {
+      timeBlock.classList.add("future");
     }
-// Create event input and save button
-const eventInput = document.createElement("input");
-eventInput.type = "text";
-eventInput.placeholder = "Enter event";
-const saveButton = document.createElement("button");
-saveButton.textContent = "Save";
 
-// Load saved events from local storage
-const savedEvent = localStorage.getItem(`event_${hour}`);
+    // Create event input and save button
+    const eventInput = document.createElement("input");
+    eventInput.type = "text";
+    eventInput.placeholder = "Enter event";
+    const saveButton = document.createElement("button");
+    saveButton.textContent = "Save";
+
+    // Load saved events from local storage
+    const savedEvent = localStorage.getItem(`event_${hour}`);
     if (savedEvent) {
       eventInput.value = savedEvent;
     }
 
-// Save event to local storage on button click
-saveButton.addEventListener("click", function() {
-localStorage.setItem(`event_${hour}`, eventInput.value);
+    // Save event to local storage on button click
+    saveButton.addEventListener("click", function() {
+      localStorage.setItem(`event_${hour}`, eventInput.value);
+    });
+
+    // Add the elements to the time block
+    timeBlock.appendChild(eventInput);
+    timeBlock.appendChild(saveButton);
+
+    // Add the time block to the container
+    container.appendChild(timeBlock);
+  }
 });
-
-// Append the elements to the time block
-timeBlock.appendChild(eventInput);
-timeBlock.appendChild(saveButton);
-
-// Append the time block to the container
-container.appendChild(timeBlock);
-
-});
-  
